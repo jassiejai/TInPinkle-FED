868e32700dc6b04068d715c16b8afe2945478869
Jassie <jasminehart94@gmail.com>
2022-02-27 17:54:26 -0800

    coomented out

--- tinpinkle/src/app/app-routing.module.ts
+++ tinpinkle/src/app/app-routing.module.ts
@@ -9,19 +9,23 @@ const routes: Routes = [
     path : ' ', component : AppComponent,
   }, {
 
-    path : 'spellbook ', component : SpellbookComponent,
+    path : 'spellbook ', 
+    component : SpellbookComponent,
 
-    children : [{
-      path: 'spellpage/:pageId' , component : SpellbookComponent
-    }]
+    //create another componet that open to specific spell
+    // children : [{
+    //   path: 'spellpage/:pageId' , component : SpellbookComponent
+    // }]
   },
   {
 
-    path : 'index ', component : IndexComponent,
+    path : 'index ', 
+    component : IndexComponent,
 
-    children : [{
-      path: 'index/:itemId' , component : IndexComponent
-    }]
+    // create another component that shows the specific item being choosen and the information on it
+    // children : [{
+    //   path: 'index/:itemId' , component : IndexComponent
+    // }]
   }
 
 
